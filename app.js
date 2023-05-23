56c35d5a44209cf84a9dc9fa77a6ce0bfed417c9
osmancangul <75526353+osmancangul@users.noreply.github.com>
2023-05-23 23:22:04 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+function yonlendir(){ 
+    window.location.assign("https://www.instagram.com/osmancangull/");
+}   
+function yonlendir2(){ 
+    window.location.assign("https://twitter.com/osmannbateman");
+}  
