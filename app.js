353daaf2c176757852e15e381f416694be2815b6
osmancangul <75526353+osmancangul@users.noreply.github.com>
2023-05-24 18:46:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ function yonlendir(){
     window.location.assign("https://www.instagram.com/osmancangull/");
 }   
 function yonlendir2(){ 
-    window.location.assign("https://twitter.com/osmannbateman");
+    window.location.assign("https://twitter.com/osmanncan_gulll");
 }  
